cb18a23d5f35cac6b99b7d703ee31114794623ca
Jayson Teano <jteano@sprout.ph>
2022-08-08 23:10:53 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const currentTask = process.env.npm_lifecycle_event;
+
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtracPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+const config = {
+  entry: "./index.js",
+
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "bundle.[fullhash].js",
+  },
+
+  // devtool: "eval-cheap-source-map",
+
+  plugins: [new HTMLWebpackPlugin({ template: "./index.html" })],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                { useBuiltIns: "usage", corejs: 3, targets: "defaults" },
+              ],
+              "@babel/preset-react",
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
+
+if (currentTask == "build") {
+  config.module.rules[0].use[0] = MiniCssExtracPlugin.loader;
+  config.plugins.push(
+    new MiniCssExtracPlugin({ filename: "main.[fullhash].css" }),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin()
+  );
+}
+
+module.exports = config;
